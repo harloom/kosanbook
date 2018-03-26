8136481004c8fa07ac5346dc34ba93dc2f6c8908
ilham solehudin <ilhamsolehudin1999@gmail.com>
2018-03-26 23:05:28 +0700

    26-03-2018

--- src/main/Main.java
+++ src/main/Main.java
@@ -30,72 +30,58 @@ public class Main {
      */        
     
     public static void main(String[] args) throws SQLException, MemberException, HargaException {
-//        int sewa = 10;
-//    Calendar now = Calendar.getInstance();
-//        int year = (now.get(Calendar.YEAR));
-//        String cyear = Integer.toString(year);
-//        int mont = (now.get(Calendar.MONTH) + 1);
-//        String cmont = Integer.toString(mont);
-//       int day  =  (now.get(Calendar.DATE) );
-//        String cday = Integer.toString(day);
-//        String date = cyear +"/"+cmont+"/"+cday;
-//        
-//
-// 
-//Calendar ex = Calendar.getInstance();
-//ex.add(Calendar.MONTH, sewa);
-//SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-//String exdate = sdf.format(ex.getTime());
-//
-//        
-//        System.out.println(date);
-//        System.out.println("+++++");
-//        System.out.println(exdate);
-        
-        
-        HargaDao harga = Database.getHargaDao();
- List<Harga> list = harga.selectallHarga();
 
- for (Harga valueHarga:list)
- {
-//     System.out.println("50Mbps = "+valueHarga.getH50mbps());
-//     System.out.println("30Mbps = "+valueHarga.getH30mbps());
-//     System.out.println("20Mbps = "+valueHarga.getH20mbps());
-//     System.out.println("vip = "+valueHarga.getHvip());
-//     System.out.println("standard = "+valueHarga.getHstandard());
-//     System.out.println("kosong = "+valueHarga.getHkosong());
-         
- }
- int tp = 0;
+        
+        
+
+
+//
+// for (Harga valueHarga:list)
+// {
+////     System.out.println("50Mbps = "+valueHarga.getH50mbps());
+////     System.out.println("30Mbps = "+valueHarga.getH30mbps());
+////     System.out.println("20Mbps = "+valueHarga.getH20mbps());
+////     System.out.println("vip = "+valueHarga.getHvip());
+////     System.out.println("standard = "+valueHarga.getHstandard());
+////     System.out.println("kosong = "+valueHarga.getHkosong());
+//         
+// }
+// int tp = 0;
  Service_member_dao member=Database.getMemberDao();
-List<Member> listmember = member.selectallmember();
+List<Member> cari = member.cariMember("agil");
 
-for(Member  value : listmember)
-{
-
-    tp+=value.getTotal();
-    
+for(Member isi:cari){
+    System.out.println(isi.getNama());
 }
-      StringBuffer sb = new StringBuffer(Integer.toString(tp));
-      if(sb.length()>=0 && sb.length()<=6){
-            sb.insert(3, ".");
-      }else if(sb.length()==7){
-          sb.insert(5, ".");
-      }else if(sb.length()==8){
-          sb.insert(2, ".");
-          sb.insert(6, ".");
-      }else if(sb.length()==9){
-          sb.insert(3, ".");
-          sb.insert(7, ".");
-      }else if(sb.length()==10){
-           sb.insert(1, ".");
-          sb.insert(5, ".");
-          sb.insert(9, ".");
-   
-      }
-    
-      System.out.println(sb); 
-System.out.println(tp);
+//  System.out.println(cari.getNama());
+        
+
+//for(Member  value : listmember)
+//{
+//
+//    tp+=value.getTotal();
+//    
+//}
+//      StringBuffer sb = new StringBuffer(Integer.toString(tp));
+//      if(sb.length()>=0 && sb.length()<=6){
+//            sb.insert(3, ".");
+//      }else if(sb.length()==7){
+//          sb.insert(5, ".");
+//      }else if(sb.length()==8){
+//          sb.insert(2, ".");
+//          sb.insert(6, ".");
+//      }else if(sb.length()==9){
+//          sb.insert(3, ".");
+//          sb.insert(7, ".");
+//      }else if(sb.length()==10){
+//           sb.insert(1, ".");
+//          sb.insert(5, ".");
+//          sb.insert(9, ".");
+//   
+//      }
+//    
+//      System.out.println(sb); 
+//System.out.println(tp);
 
     }
     
